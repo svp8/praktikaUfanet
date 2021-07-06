f6527f5e5f0b2f32d7068d9b943d157d1106d7ec
svp8 <55758407+svp8@users.noreply.github.com>
2021-07-06 16:11:38 +0500

    Update ChatController.java

--- src/main/java/com/example/webService/controller/ChatController.java
+++ src/main/java/com/example/webService/controller/ChatController.java
@@ -19,7 +19,7 @@ public class ChatController {
         final String get=idChat+"&text="+text.replace("_","%20");
         try {
 
-            getResult = Request.Get("https://api.telegram.org/bot1771180306:AAGZoZDoA5LvzsLcuAPzAI5aWT7VchiTcj4/sendMessage?chat_id="+get)
+            getResult = Request.Get("https://api.telegram.org/bot***/sendMessage?chat_id="+get)
                     .execute().returnContent();
            return getResult.asString();
 
